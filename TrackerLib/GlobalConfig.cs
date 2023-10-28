48e94672e6cb8c3ea30513fa8b033f36dc0c3ad9
petarcelar <petarcelar7@gmail.com>
2023-10-28 19:00:42 +0200

    commit - initial interface

---
+++ TrackerLib/GlobalConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLib
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConn> Connections { get; private set; }
+        public static void InitializeConns(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // TODO - create sql conn
+            }
+            if (textFiles)
+            {
+                // TODO - create txt conn
+            }
+        }
+    }
+}
